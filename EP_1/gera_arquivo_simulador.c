cd244ece056a244b9c7da7f13a3a8f84b3efafa0
IgorBarberino <igorbarberino@usp.br>
2020-09-08 18:17:35 -0300

    arquivo de simulação

---
+++ EP_1/gera_arquivo_simulador.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(int argc, char* argv[]) {
+    srand(1);
+
+    FILE* ptr;
+    ptr = fopen("simulacao.txt", "w+");
+    
+    int contador = atoi(argv[1]);
+
+    for(int i = 0; i < contador; i++) {
+        int t0 = rand()%1000;
+        int dt =rand()%1000;
+        int deadline = rand()%1000 + t0 + dt;    
+        fprintf(ptr, "processo%d %d %d %d\n", i, t0, dt, deadline);
+    }
+    
+    fclose(ptr);
+
+    return 0;
+}
